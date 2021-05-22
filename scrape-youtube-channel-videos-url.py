1901334a8a55e06e4ec07bbb9b0010ad126c5717
lattededavid <kohhyeok@gmail.com>
2021-05-22 05:16:33 +0000

    Add url validation in scrape-youtube-channel-videos-url.py

--- scrape-youtube-channel-videos-url.py
+++ scrape-youtube-channel-videos-url.py
@@ -1,36 +1,41 @@
+# https://github.com/banhao/scrape-youtube-channel-videos-url
 # scrape-youtube-channel-videos-url.py
 #_*_coding: utf-8_*_
 
-import sys, unittest, time, datetime
-import urllib.request, urllib.error, urllib.parse
+import requests
+import sys, time, datetime
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import InvalidArgumentException
 
-url = sys.argv[1]
-channelid = url.split('/')[4]
-#driver=webdriver.Firefox()
-driver=webdriver.Chrome()
+channelid = sys.argv[1]
+
+# ex. 'https://www.youtube.com/channel/UCcloDgiDqz8twby7zvG-USg/videos'
+url = 'https://www.youtube.com/channel/%s/videos' % channelid
+response = requests.get(url)
+if response.status_code != 200:
+    # ex. 'https://www.youtube.com/c/tvchanews/videos'
+    url = 'https://www.youtube.com/c/%s/videos' % channelid
+    response = requests.get(url)
+    assert response.status_code == 200
+
+driver = webdriver.Chrome()
 driver.get(url)
 time.sleep(5)
-dt=datetime.datetime.now().strftime("%Y%m%d%H%M")
+dt = datetime.datetime.now().strftime("%Y%m%d.txt")
 height = driver.execute_script("return document.documentElement.scrollHeight")
 lastheight = 0
 
 while True:
-	if lastheight == height:
-		break
-	lastheight = height
-	driver.execute_script("window.scrollTo(0, " + str(height) + ");")
-	time.sleep(2)
-	height = driver.execute_script("return document.documentElement.scrollHeight")
+    if lastheight == height:
+        break
+    lastheight = height
+    driver.execute_script("window.scrollTo(0, " + str(height) + ");")
+    time.sleep(3)
+    height = driver.execute_script("return document.documentElement.scrollHeight")
 
-user_data = driver.find_elements_by_xpath('//*[@id="video-title"]')
-for i in user_data:
-	print(i.get_attribute('href'))
-	link = (i.get_attribute('href'))
-	f = open(channelid+'-'+dt+'.list', 'a+')
-	f.write(link + '\n')
-f.close
+with open('%s-%s.list' % (channelid, dt), 'w') as fout:
+    user_data = driver.find_elements_by_xpath('//*[@id="video-title"]')
+    for i in user_data:
+        link = i.get_attribute('href')
+        print(link, file=fout)
+
+driver.close()
